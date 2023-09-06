52009658053c0532c86998a3743f7f6bcf075a07
Pedro Souza <116217127+UnderworldBlues@users.noreply.github.com>
2023-09-05 22:30:58 -0300

    Update sorting.c
    added mergeSort back

--- atividade-1/sorting.c
+++ atividade-1/sorting.c
@@ -82,6 +82,58 @@ void shellSort (int *v, int n)
     } while (h != 1);
 }
 
+void mergeSort (int *v, int n)
+{
+    int *w; 
+    w = (int *)malloc(n * sizeof(int));
+    mergeSortRec(v, w, 0, n - 1);
+    free(w);
+}
+
+void mergeSortRec (int *v, int *w, int ini, int fim)
+{
+    int meio;
+    if (ini < fim)
+    {
+        meio = (ini + fim) / 2;
+        mergeSortRec(v, w, ini, meio);
+        mergeSortRec(v, w, meio + 1, fim);
+        merge(v, w, ini, meio, fim);
+    }
+}
+
+void merge (int *v, int *w, int ini, int meio, int fim)
+{
+    int i, j, k;
+    i = ini;
+    j = meio + 1;
+    k = ini;
+    while ((i <= meio) && (j <= fim))
+    {
+        if (v[i] < v[j])
+        {
+            w[k++] = v[i++];
+        }
+        else
+        {
+            w[k++] = v[j++];
+        }
+    }
+    while (i <= meio)
+    {
+        w[k++] = v[i++];
+    }
+    while (j <= fim)
+    {
+        w[k++] = v[j++];
+    }
+    for (i = ini; i <= fim; i++)
+    {
+        v[i] = w[i];
+    }
+
+}
+
 void ABCsort (int *v, int i, int j) 
 {
     if(v[i] > v[j])
